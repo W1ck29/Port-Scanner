c64176fb1689b524d8cf0a26d791f4876551a24d
Janek Wajda <64981888+W1ck29@users.noreply.github.com>
2023-09-04 00:04:27 +0200

    Added comment

--- ScannerClass.py
+++ ScannerClass.py
@@ -4,6 +4,7 @@ class Scanner():
         self.socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def scan_ports(self, ips, port):
+        # check if port is open
         for ip in ips:
             result = self.socket_obj.connect_ex((ip, port))
             if result == 0:
